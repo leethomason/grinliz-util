928e33a645916290395b3be9859d70816b69fb3c
Lee Thomason <leethomason@gmail.com>
2022-11-16 20:01:01 -0800

    new random test

--- grinliz-util.cpp
+++ grinliz-util.cpp
@@ -5,9 +5,33 @@
 #include "grinliz/glstringpool.h"
 #include "grinliz/gltree.h"
 #include "grinliz/glparser.h"
+#include "grinliz/glrandom.h"
+
+int CountBits(uint32_t a)
+{
+	int count = 0;
+	for (int i = 0; i < 32; ++i)
+		if (a & (1 << i))
+			++count;
+	return count;
+}
+
+bool TestRandom()
+{
+//	printf("%d %d %d\n", 
+//		CountBits(grinliz::Random::Mix(0)), 
+//		CountBits(grinliz::Random::Mix(1)), 
+//		CountBits(grinliz::Random::Mix(3)));
+	printf("%x %x %x\n",
+		grinliz::Random::Mix(1),
+		grinliz::Random::Mix(2),
+		grinliz::Random::Mix(3));
+	return true;
+}
 
 int main()
 {
+	TestRandom();
 	grinliz::TestContainers();
 	grinliz::ConsumerProducerQueueTest(clock());
 	grinliz::TestRect();
